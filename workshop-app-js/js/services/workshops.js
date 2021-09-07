0b730edddc4b8ceac73a4bd62e76abc68a5acecc
Raj Kshirsagar <kshirsagar.2798@gmail.com>
2021-09-07 14:53:11 +0530

    Vue started

---
+++ workshop-app-js/js/services/workshops.js
@@ -0,0 +1,13 @@
+function getWorkshops() {
+  return axios
+    .get(`https://workshops-server.herokuapp.com/workshops/`)
+    .then((response) => response.data);
+}
+
+function getWorkshopById(id) {
+  return axios
+    .get(`https://workshops-server.herokuapp.com/workshops/${id}`)
+    .then((response) => response.data);
+}
+
+export { getWorkshops, getWorkshopById };
