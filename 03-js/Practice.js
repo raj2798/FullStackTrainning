3f7e5be0b8d9eb874890b422dc2d1af2716994cc
Raj Kshirsagar <kshirsagar.2798@gmail.com>
2021-09-13 12:12:26 +0530

    VueExcercises

---
+++ 03-js/Practice.js
@@ -0,0 +1,11 @@
+const items = [
+  { name: "Pen", price: 20 },
+  { name: "Pencil", price: 5 },
+  { name: "Eraser", price: 15 },
+];
+
+function iterator(x) {
+  console.log(x);
+}
+
+items.forEach(iterator);
--- 07-workshop-app-vue/workshop-app/src/components/navbar.vue
+++ 07-workshop-app-vue/workshop-app/src/components/navbar.vue
@@ -3,11 +3,15 @@
   <nav class="navbar navbar-expand navbar-light bg-light">
     <div class="container px-2">
       <ul class="nav navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home </a>
+        <li class="nav-item ">
+          <router-link class="nav-link" to="/" active-class="active"
+            >Home
+          </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/workshops">List of Workshop</a>
+          <router-link class="nav-link" to="/workshops" active-class="active"
+            >List of Workshop</router-link
+          >
         </li>
       </ul>
     </div>
