f6ff824849d6e84a6ee2ce4d332ddaa4a7769661
Raj Kshirsagar <kshirsagar.2798@gmail.com>
2021-09-30 13:16:27 +0530

    Logical Operators

--- 11-MongoDB/02-excercise-06.js
+++ 11-MongoDB/02-excercise-06.js
@@ -1,70 +1,163 @@
-//1. Question1
+// For this exercise, you will need to import the shows collection into some DB first. Then try
+// the following.
+// a) Comparison (i.e. relational) operators (including equality, i.e. exact match)
+// i. Find shows that are less than one hour duration
 db.shows.find({
   runtime: {
-    $lte: 60,
+    $lt: 60,
   },
 });
 
-//2. Question2
+// ii. Find shows with runtime between 30 and 60 minutes
 db.shows.find({
   runtime: {
-    $lte: 60,
+    $lt: 60,
     $gt: 30,
   },
 });
 
-//3. Question3
+// iii. Find shows with average rating at least 8
 db.shows.find({
   "rating.average": {
     $gte: 8,
   },
 });
-//4. Question4
+
+// iv. Find shows with rating between 8 and 9
 db.shows.find({
-  genres: "horror",
+  "rating.average": {
+    $gte: 8,
+    $lte: 9,
+  },
 });
 
-//5.Question5
+// v. Find shows in horror genre (array of string, but query is written like genres field is a string)
+// genres: 'Horror' (one of the genres is 'Horror')
 db.shows.find({
-  genres: ["Horror"],
+  genres: "Horror",
 });
 
-//in oprator
+// vi. Find shows which have ONLY horror as their genre
+// genres: [ 'Drama', 'Fantasy', 'Horror' ] (genres shhould have exctly these items in this order)
+db.shows.find({
+  genres: ["Drama", "Fantasy", "Horror"],
+});
+
+// Special comparison operators that match within a list of values - $in, $nin (use these operators in your solution)
+// i) Find shows that have one of Drama or Horror as genres in them
 db.shows.find({
   genres: {
     $in: ["Horror", "Drama"],
   },
 });
 
+// Extra query: Find shows with runtime either 45 or 50 minutes
 db.shows.find({
   runtime: {
     $in: [45, 50],
   },
 });
 
+// ii) Find shows that are of type Animation or Reality
+// $in -> one of the mentioned values
 db.shows.find({
   type: {
     $in: ["Animation", "Reality"],
   },
 });
-//nin (not in)
+
+// iii) Find shows that have neither Drama nor Horror as genres in them
+// $nin -> none of the mentioned values
+db.shows.find({
+  genres: {
+    $nin: ["Drama", "Horror"],
+  },
+});
+
+// iv) Find shows that are neither of type Animation, nor Reality
+db.shows.find({
+  type: {
+    $nin: ["Animation", "Reality"],
+  },
+});
+
+// v) Find shows that are not running on the network HBO, nor FOX
 db.shows.find({
   "network.name": {
     $nin: ["HBO", "FOX"],
   },
 });
 
-//Logical OPerators
+// Logical operators
+
+// i) Find shows that have one of Drama or Horror as genres in them (try this
+// without using $in)
+// ( genres === 'Drama' ) || ( genres === 'Horror' )
 db.shows.find({
-  $or: [{ genres: "Drama" }, { genres: "Horror" }],
+  $or: [
+    {
+      genres: "Drama",
+    },
+    {
+      genres: "Horror",
+    },
+  ],
 });
 
-db.shows
-  .find({
-    genres: {
-      $not: {
-        $in: ["Drama", "Horror"],
-      },
+// ii) Find shows that have neither Drama nor Horror as genres in them (try this with
+// $not and $in ("none of these" ($nin) is same as "not one of these" ($not, $in))
+db.shows.find({
+  genres: {
+    $not: {
+      $in: ["Drama", "Horror"],
     },
-  })
-  .count();
+  },
+});
+
+// iii) Find shows that have BOTH Drama and Horror as their genre
+// ( genres === 'Drama' ) && ( genres === 'Horror' )
+db.shows.find({
+  $and: [
+    {
+      genres: "Drama",
+    },
+    {
+      genres: "Horror",
+    },
+  ],
+});
+
+// this will end up giving the documents with genre Horror (Drama is ignored)
+// db.shows.find(
+//     {
+//         genres: 'Drama',
+//         genres: 'Horror'
+//     }
+// );
+
+// can this query be simplified? - yes
+db.shows.find({
+  $and: [
+    {
+      genres: "Drama",
+    },
+    {
+      runtime: 45,
+    },
+  ],
+});
+
+// this is simple
+db.shows.find({
+  genres: "Drama",
+  runtime: 45,
+});
+
+// iv) Find shows that have genre Drama but not Horror. Again, find shows that have
+// genre Horror but not Drama.
+// Assignment
+
+// v) Find shows that do not have at least one of Drama or Horror as their genre
+// (i.e. show could have Drama but not Horror, Horror but not Drama, or neither
+// Drama not Horror)
+// Assignment
