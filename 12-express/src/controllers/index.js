159090949b09dfcb92d09b549c6659123ea5d260
Raj Kshirsagar <kshirsagar.2798@gmail.com>
2021-10-07 08:23:14 +0530

    Add Home.html in public src

--- 12-express/src/controllers/index.js
+++ 12-express/src/controllers/index.js
@@ -1,23 +1,25 @@
+const path = require("path");
 const home = (req, res) => {
   //sets the response header - 'Content-Type': 'text/html' (res.end() does not do so)
-  res.send(
-    `
-        <!doctype html>
-                <html>
-                    <head>
-                        <title>Workshops app</title>
-                    </head>
-                    <body>
-                        <h1>Workshops app</h1>
-                        <hr />
-                        <p>
-                            You can view/update workshops, session here, and also add new ones.
-                        </p>
-                    </body>
-                </html>
-    
-          `
-  );
+  // res.send(
+  //   `
+  //       <!doctype html>
+  //               <html>
+  //                   <head>
+  //                       <title>Workshops app</title>
+  //                   </head>
+  //                   <body>
+  //                       <h1>Workshops app</h1>
+  //                       <hr />
+  //                       <p>
+  //                           You can view/update workshops, session here, and also add new ones.
+  //                       </p>
+  //                   </body>
+  //               </html>
+
+  //         `
+  // );
+  res.sendFile(path.join(__dirname, "..", "public/home.html"));
 };
 
 const meetings = (req, res) => {
